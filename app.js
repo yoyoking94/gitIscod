344024092cb56234aa0209c8ab106a27aeb5286e
Yovish MOONESAMY <yoyoking94@MacBook-Pro-de-Yovish.local>
2025-04-28 13:25:02 +0200

    C3 : mise à jour taille fenetre et ajustement du texte des constantes

--- app.js
+++ app.js
@@ -2,4 +2,4 @@ const HELLO_WORLD_TITLE = document.getElementById("title");
 const HELLO_WORLD_TEXT = document.getElementById("text");
 
 HELLO_WORLD_TITLE.innerText = "Title Hello world";
-HELLO_WORLD_TEXT.innerText = "Hello world text";
+HELLO_WORLD_TEXT.innerText = "Prout";
