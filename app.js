e8ef894d590c06dbf3c90c12135115f2e94886dd
Yovish MOONESAMY <yoyoking94@MacBook-Pro-de-Yovish.local>
2025-04-28 13:20:16 +0200

    C2 : extraction des constantes

---
+++ app.js
@@ -0,0 +1,5 @@
+const HELLO_WORLD_TITLE = document.getElementById("title");
+const HELLO_WORLD_TEXT = document.getElementById("text");
+
+HELLO_WORLD_TITLE.innerText = "Title Hello world";
+HELLO_WORLD_TEXT.innerText = "Hello world text";
